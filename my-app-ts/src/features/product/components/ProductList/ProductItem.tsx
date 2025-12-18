59bbf37f4aaa1970977678297f4fa1b43b6efdc5
TaichiOnishi <9003325675@g.ecc.u-tokyo.ac.jp>
2025-12-19 01:16:41 +0900

    fix likes

--- my-app-ts/src/features/product/components/ProductList/ProductItem.tsx
+++ my-app-ts/src/features/product/components/ProductList/ProductItem.tsx
@@ -42,7 +42,7 @@ export const ProductItem = ({ product, currentUserId }: Props) => {
         const user = auth.currentUser;
         const token = user ? await user.getIdToken() : '';
 
-        const response = await fetch(`https://hackathon-backend-80731441408.europe-west1.run.app/products/like?id=${product.id}`, {
+        const response = await fetch(`https://hackathon-backend-80731441408.europe-west1.run.app/products/${product.id}/like`, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${token}`
